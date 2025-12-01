878f3da2ef9caa75cb9f64c61df1f8c6632ec9cf
Failure Coders <122425425+FailureCoder@users.noreply.github.com>
2025-12-01 10:11:49 +0530

    Update positions table schema: Changed entry_type values to include 'short', added profit_loss and learning columns.

--- db/scripts/table.sql
+++ db/scripts/table.sql
@@ -1,10 +1,16 @@
 create table positions (
   id uuid default uuid_generate_v4() primary key,
   stock text not null,
-  entry_type text check (entry_type in ('buy', 'sell')) not null,
+  entry_type text check (entry_type in ('buy', 'short')) not null,
   purchase_price numeric,
   target numeric,
   stop_loss numeric,
   quantity numeric,
+  profit_loss numeric,
+  learning text,
   created_at timestamptz default now()
 );
+
+
+
+
